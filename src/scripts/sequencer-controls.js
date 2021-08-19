5accd0fe8dc37406c8fc3f76e565cc89ff0a3df9
jd <johndetora@gmail.com>
2021-08-19 17:19:10 -0400

    import render note

--- src/scripts/sequencer-controls.js
+++ src/scripts/sequencer-controls.js
@@ -3,6 +3,7 @@
 // ------------------------- //
 import { notes } from './note-data';
 import { currentScale } from './note-data';
+import { renderNoteMeters } from './render-note-meters';
 
 export function sequencerInput() {
     const stepContainer = document.querySelector('#steps');
@@ -40,16 +41,6 @@ export function sequencerInput() {
                 asciiRepeater[i].innerHTML = '│-│' + '<br>' + '│-│' + '<br>' + '│-│' + '<br>' + '│o│' + '<br>';
             }
         }
-    }
-
-    // ///////  Update Note Meters  ////////
-    function renderNoteMeters(inputVal) {
-        const BARS_MAX = 12; // Max slider value for note meters
-        let top = '_' + '<br>';
-        let bottom = '^' + '<br>';
-        let row = '|░|' + '<br>';
-        let filled = '|▓|' + '<br>';
-        return top + row.repeat(BARS_MAX - inputVal) + filled.repeat(inputVal) + filled + bottom;
     }
 
     // // Repeater ascii-animation. There's probably a better way to do this but it works.
